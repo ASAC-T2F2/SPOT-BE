225b163fafa906d07cd844ed942c0695968e9800
Minwoo Lee <rkdxhd97@naver.com>
2024-07-09 15:30:34 +0900

    Create Project

---
+++ src/main/java/T2F2/SPOT/SpotApplication.java
@@ -0,0 +1,13 @@
+package T2F2.SPOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotApplication.class, args);
+	}
+
+}
