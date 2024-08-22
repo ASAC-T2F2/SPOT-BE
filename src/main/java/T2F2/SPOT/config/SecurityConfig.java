c147adc87fe65b9e63d67105555264227aa926eb
Hyeon Jun <rnclf1005@naver.com>
2024-08-22 16:52:16 +0900

    query dsl 심폐소생술
    - post 생성
    - post 수정
    - post 상태 변경
    - post query dsl 검색

--- src/main/java/T2F2/SPOT/config/SecurityConfig.java
+++ src/main/java/T2F2/SPOT/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/auth/join", "/email/**").permitAll()
+                        .requestMatchers("/login", "/", "/auth/join", "/email/**", "/api/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/token/reissue").permitAll()
                         .anyRequest().authenticated());
--- src/main/java/T2F2/SPOT/domain/category/entity/Category.java
+++ src/main/java/T2F2/SPOT/domain/category/entity/Category.java
@@ -3,11 +3,13 @@ package T2F2.SPOT.domain.category.entity;
 import T2F2.SPOT.domain.post.entity.Post;
 import T2F2.SPOT.util.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 public class Category extends BaseEntity {
 
     @Id
