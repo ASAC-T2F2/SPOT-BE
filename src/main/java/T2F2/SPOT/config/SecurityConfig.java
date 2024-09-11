17646609279bd1a9547dc91556ec4787f7d41bf8
minwoo lee <rkdxhd97@naver.com>
2024-09-11 23:37:34 +0900

    Feat - Swagger 세팅 및 기본작성
    - API path 변경
      - 사용자 권한이 필요한 API는 /api 경로 앞에 추가
    - Swagger 기본 세팅
      - JWT 적용
    - Spring Security Swagger 기본 경로 권한 설정
    - CORS 모든 경로 추가

--- src/main/java/T2F2/SPOT/config/SecurityConfig.java
+++ src/main/java/T2F2/SPOT/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -62,15 +63,14 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
+        http
+                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()));
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/auth/join", "/email/**", "/api/**", "/aws/**", "/note/**", "/noteRoom/**").permitAll()
+                        .requestMatchers("/login", "/","/swagger-ui/**", "/v3/api-docs/**", "/auth/join","/token/reissue", "/email/**", "/aws/**", "/note/**", "/noteRoom/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/wish/add", "/wish/cancel").hasAuthority("USER")
-                        .requestMatchers("/review/**").hasAuthority("USER")
-                        .requestMatchers("/user/**").hasAuthority("USER")
-                        .requestMatchers("/token/reissue").permitAll()
+                        .requestMatchers("/api/**").hasAuthority( "USER")
                         .anyRequest().authenticated());
 
         http
--- src/main/java/T2F2/SPOT/domain/review/controller/ReviewController.java
+++ src/main/java/T2F2/SPOT/domain/review/controller/ReviewController.java
@@ -7,6 +7,11 @@ import T2F2.SPOT.domain.review.exception.ReviewException;
 import T2F2.SPOT.domain.review.service.ReviewService;
 import T2F2.SPOT.domain.user.dto.CustomUserDetails;
 import T2F2.SPOT.domain.user.exception.UserExceptions;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -18,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/review")
+@RequestMapping("/api/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
@@ -28,6 +33,14 @@ public class ReviewController {
     }
 
     @PostMapping("/write")
+    @Operation(summary = "리뷰 작성", description = "현재 인증된 사용자가 상대방에 대해 리뷰를 작성하는 API. 판매자, 구매자 모두 해당 API를 사용 가능하다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "리뷰 작성 성공",
+                    content = @Content(schema = @Schema(implementation = ReviewResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
+            @ApiResponse(responseCode = "404", description = "사용자 또는 게시물을 찾을 수 없음"),
+            @ApiResponse(responseCode = "400", description = "리뷰가 이미 존재함")
+    })
     public ResponseEntity<?> write(@RequestBody CreateReviewRequest writeReviewRequest) {
 
         try {
