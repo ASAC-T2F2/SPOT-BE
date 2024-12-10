58378739e0e9ee3b4f2ec40d47f33b7890755e1e
podol0207 <audrhks0207@naver.com>
2024-12-10 16:39:36 +0900

    Fix - get, post API 분리
    - 사진 다룬로드와 사진 업로드로 분리

--- src/main/java/T2F2/SPOT/util/AWS/AWSController.java
+++ src/main/java/T2F2/SPOT/util/AWS/AWSController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -19,11 +16,22 @@ public class AWSController {
 
     private final AWSService awsService;
 
-    @GetMapping("file/{filename}")
-    public ResponseEntity<String> getFile(@PathVariable(value = "filename") String filename) throws IOException {
-
-        String url = awsService.getPresignUrl(filename);
-
-        return new ResponseEntity<>(url, HttpStatus.OK);
+    @GetMapping("/geturl")
+    public ResponseEntity<String> getS3Url(@RequestParam String filename) {
+        String url = awsService.createPresignedGetUrl(filename);
+        return ResponseEntity.ok(url);
     }
+
+    @GetMapping("/posturl")
+    public ResponseEntity<String> getPostUrl(@RequestParam String filename) {
+        String url = awsService.createPresignedUrl(filename);
+        return ResponseEntity.ok(url);
+    }
+//    @GetMapping("file/{filename}")
+//    public ResponseEntity<String> getFile(@PathVariable(value = "filename") String filename) throws IOException {
+//
+//        String url = awsService.(filename);
+//
+//        return new ResponseEntity<>(url, HttpStatus.OK);
+//    }
 }
--- src/main/java/T2F2/SPOT/util/AWS/AWSService.java
+++ src/main/java/T2F2/SPOT/util/AWS/AWSService.java
@@ -26,47 +26,94 @@ public class AWSService {
     private final S3Client s3Client;
     private final S3Presigner presigner;
 
-    public String getPresignUrl(String filename){
-        if(filename == null || filename.equals("")) {
-            return null;
+    public String createPresignedGetUrl(String fileName) {
+
+        if (fileName == null || fileName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Filename must not be null or empty");
+        }
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
+
+            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(5)) // URL 만료시간
+                    .getObjectRequest(objectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(presignRequest);
+            log.info("Presigned URL : [{}]", presignedRequest.url().toString());
+            log.info("HTTP method : [{}]", presignedRequest.httpRequest().method());
+
+            return presignedRequest.url().toExternalForm();
+    }
+
+    public String createPresignedUrl(String fileName) {
+
+        if (fileName == null || fileName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Filename must not be null or empty");
         }
 
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(filename)
-                .build();
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(filename)
-                .build();
+            PutObjectRequest objectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
 
-        log.info(getObjectRequest.toString());
-        log.info(putObjectRequest.toString());
+            PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                    .signatureDuration(Duration.ofMinutes(10))  // The URL expires in 10 minutes.
+                    .putObjectRequest(objectRequest)
+                    .build();
 
-        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(5)) // presignedURL 5분간 접근 허용
-                .getObjectRequest(getObjectRequest)
-                .build();
 
-        PutObjectPresignRequest putObjectPresignRequest = PutObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(5))
-                .putObjectRequest(putObjectRequest)
-                .build();
+            PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(presignRequest);
+            String myURL = presignedRequest.url().toString();
+            log.info("Presigned URL to upload a file to: [{}]", myURL);
+            log.info("HTTP method: [{}]", presignedRequest.httpRequest().method());
 
-        log.info(getObjectPresignRequest.toString());
-        log.info(putObjectPresignRequest.toString());
-
-        PresignedGetObjectRequest presignedGetObjectRequest = presigner
-                .presignGetObject(getObjectPresignRequest);
-
-        PresignedPutObjectRequest presignedPutObjectRequest = presigner
-                .presignPutObject(putObjectPresignRequest);
-
-        String url = presignedPutObjectRequest.url().toString();
-//        String url = presignedGetObjectRequest.url().toString();
-        log.info(url);
-//        presigner.close(); // presigner를 닫고 획득한 모든 리소스를 해제
-        return url;
+            return presignedRequest.url().toExternalForm();
     }
+
+//    public String getPresignUrl(String filename){
+//        if(filename == null || filename.equals("")) {
+//            return null;
+//        }
+//
+//        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(filename)
+//                .build();
+//        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(filename)
+//                .build();
+//
+//        log.info(getObjectRequest.toString());
+//        log.info(putObjectRequest.toString());
+//
+//        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+//                .signatureDuration(Duration.ofMinutes(5)) // presignedURL 5분간 접근 허용
+//                .getObjectRequest(getObjectRequest)
+//                .build();
+//
+//        PutObjectPresignRequest putObjectPresignRequest = PutObjectPresignRequest.builder()
+//                .signatureDuration(Duration.ofMinutes(5))
+//                .putObjectRequest(putObjectRequest)
+//                .build();
+//
+//        log.info(getObjectPresignRequest.toString());
+//        log.info(putObjectPresignRequest.toString());
+//
+//        PresignedGetObjectRequest presignedGetObjectRequest = presigner
+//                .presignGetObject(getObjectPresignRequest);
+//
+//        PresignedPutObjectRequest presignedPutObjectRequest = presigner
+//                .presignPutObject(putObjectPresignRequest);
+//
+//        String url = presignedPutObjectRequest.url().toString();
+////        String url = presignedGetObjectRequest.url().toString();
+//        log.info(url);
+////        presigner.close(); // presigner를 닫고 획득한 모든 리소스를 해제
+//        return url;
+//    }
 
 }
